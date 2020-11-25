c3e9cb15ddeea59e3dcec33ed39356295a4e510e
Lenovo <2488543083@qq.com>
2020-11-25 10:35:03 +0800

    创建git本地仓库2

--- src/main/java/cn/kgc/git/hello.java
+++ src/main/java/cn/kgc/git/hello.java
@@ -3,5 +3,6 @@ package cn.kgc.git;
 public class hello {
     public static void main(String[] args) {
         System.out.println("你好 我是 git!!!!");
+        System.out.println("我又上传了一个git ！！！！");
     }
 }
